bda4879b095ef4e7b182225bec0dc114f457159f
AndrzejZaba <andrzejzaba@onet.pl>
2023-08-06 12:10:51 +0200

    first commands

--- Diary/ViewModels/MainViewModel.cs
+++ Diary/ViewModels/MainViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Diary.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Diary.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            RefreshStudentCommand = new RelayCommand(RefreshStudents, CanRefreshStudents);
+        }
+
+        public ICommand RefreshStudentCommand { get; set; }
+
+        private void RefreshStudents(object obj)
+        {
+            MessageBox.Show("RefreshStudent");
+        }
+
+        private bool CanRefreshStudents(object obj)
+        {
+            return true;
+        }
+
+        
+
+        
     }
 }

a4144d3b0dc9719db9a73f5414238f0dbd567d64
AndrzejZaba <andrzejzaba@onet.pl>
2023-08-09 18:28:47 +0200

    MainViewModel update

--- Diary/ViewModels/MainViewModel.cs
+++ Diary/ViewModels/MainViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Diary.Commands;
+using Diary.Models;
+using Diary.Views;
+using MahApps.Metro.Controls.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,23 +17,124 @@ namespace Diary.ViewModels
     {
         public MainViewModel()
         {
-            RefreshStudentCommand = new RelayCommand(RefreshStudents, CanRefreshStudents);
+            AddStudentCommand = new RelayCommand(AddEditStudent);
+            EditStudentCommand = new RelayCommand(AddEditStudent, CanEditDeleteStudent);
+            DeleteStudentCommand = new AsyncRelayCommand(DeleteStudent, CanEditDeleteStudent);
+            RefreshStudentCommand = new RelayCommand(RefreshStudents);
+            RefreshDiary();
+
+            InitGroups();
         }
 
         public ICommand RefreshStudentCommand { get; set; }
+        public ICommand AddStudentCommand { get; set; }
+        public ICommand EditStudentCommand { get; set; }
+        public ICommand DeleteStudentCommand { get; set; }
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set 
+            { 
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set 
+            { 
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set 
+            { 
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Group> _groups;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _groups; }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void AddEditStudent(object obj)
+        {
+            var addEditStudentWindow = new AddEditStudentView(obj as Student);
+            addEditStudentWindow.Closed += addEditStudentWindow_Closed;
+            addEditStudentWindow.ShowDialog();
+        }
+
+        private void addEditStudentWindow_Closed(object sender, EventArgs e)
+        {
+            RefreshDiary();
+        }
+
+        private async Task DeleteStudent(object obj)
+        {
+            var metroWindow = Application.Current.MainWindow as MainWindow;
+            var dialog = await metroWindow.ShowMessageAsync(
+                "Usuwanie ucznia", 
+                $"Czy na pewno chcesz usunąć ucznia {SelectedStudent.FirstName} {SelectedStudent.LastName}?", 
+                MessageDialogStyle.AffirmativeAndNegative);
+
+            if (dialog != MessageDialogResult.Affirmative)
+                return;
+
+            //usuwanie ucznia z bazy danych
+        }
+
+        private bool CanEditDeleteStudent(object obj)
+        {
+            return SelectedStudent != null;
+        }
 
         private void RefreshStudents(object obj)
         {
-            MessageBox.Show("RefreshStudent");
+            RefreshDiary();
         }
 
-        private bool CanRefreshStudents(object obj)
+        private void InitGroups()
         {
-            return true;
+            Groups = new ObservableCollection<Group>
+            {
+                new Group {Id = 0, Name="Wszytskie"},
+                new Group {Id = 1, Name="1A"},
+                new Group {Id = 2, Name="2A"}
+            };
+
+            SelectedGroupId = 0;
         }
 
-        
-
-        
+        private void RefreshDiary()
+        {
+            Students = new ObservableCollection<Student>
+            {
+                new Student { FirstName = "Kazimierz", LastName="Kowalski", Group= new Group{Id = 1, Name = "3A"} },
+                new Student { FirstName = "Marek", LastName="Nowak", Group= new Group{Id = 1, Name = "3A"} },
+                new Student { FirstName = "Max", LastName="Linda", Group= new Group{Id = 2, Name = "3B"} }
+            };
+        }
     }
 }
